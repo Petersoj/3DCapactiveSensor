1cc823311982e44f5187d8114b3d4ca5c739da7e
Petersoj <Petersoj@users.noreply.github.com>
2021-04-30 13:16:25 -0600

    Finish CursorController implementation

--- client/View/CursorController.cs
+++ client/View/CursorController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows.Forms;
 using System.Drawing;
+using System.Runtime.InteropServices;
 
 namespace Client3DCapacitiveSensor.View {
 
@@ -32,17 +33,41 @@ namespace Client3DCapacitiveSensor.View {
             screenHeight = primaryScreenBounds.Height;
         }
 
+        /// <summary>
+        /// Sets the X,Y coordinates of the cursor.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
         public void SetXY(int x, int y) {
             Cursor.Position = new Point(
-                (x / maxInputX) * screenWidth + minScreenX,
-                (y / maxInputY) * screenHeight + minScreenY);
+                (int)((double)x / maxInputX * screenWidth + minScreenX),
+                (int)((double)y / maxInputY * screenHeight + minScreenY));
         }
 
+        /// <summary>
+        /// Simulates the user's left mouse button presseing down.
+        /// </summary>
         public void LeftButtonDown() {
+            mouse_event((int)MouseEventFlags.LeftDown, GetX(), GetY(), 0, 0);
         }
 
+        /// <summary>
+        /// Simulates the user's left mouse button presseing up.
+        /// </summary>
         public void LeftButtonUp() {
+            mouse_event((int)MouseEventFlags.LeftUp, GetX() + 100, GetY() + 100, 0, 0);
         }
+
+        /// <summary>
+        /// <see href="https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-mouse_event"/>
+        /// </summary>
+        /// <param name="dwFlags"></param>
+        /// <param name="dx"></param>
+        /// <param name="dy"></param>
+        /// <param name="dwData"></param>
+        /// <param name="dwExtraInfo"></param>
+        [DllImport("user32.dll")]
+        private static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
 
         /// <summary>
         /// Gets the X value of the cursor.
@@ -58,6 +83,19 @@ namespace Client3DCapacitiveSensor.View {
         /// <returns></returns>
         public int GetY() {
             return Cursor.Position.Y;
+        }
+
+        /// <summary>
+        /// Bit fields for <see cref="mouse_event(int, int, int, int, int)"/>.
+        /// </summary>
+        [Flags]
+        private enum MouseEventFlags {
+            LeftDown = 0x00000002,
+            LeftUp = 0x00000004,
+            MiddleDown = 0x00000020,
+            MiddleUp = 0x00000040,
+            RightDown = 0x00000008,
+            RightUp = 0x00000010
         }
     }
 }
